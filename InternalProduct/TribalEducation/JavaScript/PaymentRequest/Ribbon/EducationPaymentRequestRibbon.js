2ad3aeb8a2991e54df8363c2ce8dc8949a31cb74
kstudsrud7 <kjstudsrud@gmail.com>
2024-09-19 14:53:30 -0500

    9.19 UPDATE

--- InternalProduct/TribalEducation/JavaScript/PaymentRequest/Ribbon/EducationPaymentRequestRibbon.js
+++
@@ -1,7 +0,0 @@
-function mySampleFunction(primaryControl, selectedControl, selectedEntityTypeName, primaryControlId) {
-    console.log('primaryControl:', primaryControl);
-    console.log('selectedControl:', selectedControl);
-    console.log('entityName:', selectedEntityTypeName);
-    console.log('formName:', primaryControlId);
-    console.log('primaryControlId:', primaryControlId);
-}
